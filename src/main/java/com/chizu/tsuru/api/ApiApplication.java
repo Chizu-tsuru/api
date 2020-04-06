c36d37cc3775272daca2eb553266cc5a82199d26
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-04-06 16:30:12 +0200

    Refactor code to make it more coherent. Added base test

--- src/main/java/com/chizu/tsuru/api/ApiApplication.java
+++ src/main/java/com/chizu/tsuru/api/ApiApplication.java
@@ -1,35 +1,13 @@
 package com.chizu.tsuru.api;
 
-import com.chizu.tsuru.api.Entities.Location;
-import com.chizu.tsuru.api.services.CalculatorService;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(ApiApplication.class, args);
-        ArrayList<Location> locations = new ArrayList<>();
-        CalculatorService cs = new CalculatorService();
-        locations.add(Location.builder()
-                .latitude(0.0)
-                .longitude(1.0)
-                .build());
-        locations.add(Location.builder()
-                .latitude(0.0)
-                .longitude(2.0)
-                .build());
-        locations.add(Location.builder()
-                .latitude(0.0)
-                .longitude(3.0)
-                .build());
-        locations.add(Location.builder()
-                .latitude(0.0)
-                .longitude(4.0)
-                .build());
-        System.out.println("avg: " + cs.minAvgDist(locations));
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
--- src/main/java/com/chizu/tsuru/api/services/CalculatorService.java
+++ src/main/java/com/chizu/tsuru/api/services/CalculatorService.java
@@ -6,16 +6,12 @@ import java.util.ArrayList;
 
 public class CalculatorService {
 
-    public double minAvgDist(ArrayList<Location> p) {
-        ArrayList<Double> dist = listDist(p);
+    public double minAvgDist(ArrayList<Location> LocationList) {
+        ArrayList<Double> dist = listDist(LocationList);
         return dist
                 .stream()
                 .reduce(0.0, Double::sum)
                 / dist.size();
-    }
-
-    private double calcDist(Location a, Location b) {
-        return Math.pow((a.getLatitude() - b.getLatitude()), 2) + Math.pow((a.getLongitude() - b.getLongitude()), 2);
     }
 
     private ArrayList<Double> listDist(ArrayList<Location> p) {
@@ -25,7 +21,7 @@ public class CalculatorService {
             d = -1;
             for (int j = 0; j < p.size(); j++) {
                 if (i != j) {
-                    c = calcDist(p.get(i), p.get(j));
+                    c = convertLocationsToKm(p.get(i), p.get(j));
                     if (c < d || d == -1)
                         d = c;
                 }
@@ -35,12 +31,12 @@ public class CalculatorService {
         return dist;
     }
 
-    public double LongLatToKm(double lat1, double lon1, double lat2, double lon2) {
+    private double convertLocationsToKm(Location a, Location b) {
         double p = 0.017453292519943295;    // Math.PI / 180
-        double a = 0.5 - Math.cos((lat2 - lat1) * p) / 2 +
-                Math.cos(lat1 * p) * Math.cos(lat2 * p) *
-                        (1 - Math.cos((lon2 - lon1) * p)) / 2;
+        double res = 0.5 - Math.cos((b.getLatitude() - a.getLatitude()) * p) / 2 +
+                Math.cos(a.getLatitude() * p) * Math.cos(b.getLatitude() * p)
+                        * (1 - Math.cos((b.getLongitude() - a.getLongitude()) * p)) / 2;
 
-        return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+        return 12742 * Math.asin(Math.sqrt(res)); // 2 * R; R = 6371 km
     }
 }
