2777be385f06aeb3bba1848ef0136416be892495
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-05-21 15:28:04 +0200

    Restructure + Unit testing

--- src/main/java/com/chizu/tsuru/api/workspaces/controllers/WorkspaceController.java
+++ src/main/java/com/chizu/tsuru/api/workspaces/controllers/WorkspaceController.java
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
 public class WorkspaceController {
 
     private final WorkspaceService workspaceService;
-    private final double ACCURACY = 1000000;
+
     private final MapService mapService;
     private final ClusterService clusterService;
 
@@ -49,79 +49,13 @@ public class WorkspaceController {
 
     @PostMapping
     public ResponseEntity<Void> CreateWorkspace(@Validated @RequestBody CreateWorkspaceDTO workspace) {
-        double latDiff;
-        double longDiff;
-        double squareSize;
-        double clusterMinLat;
-        double clusterMaxLat;
-        double clusterMinLong;
-        double clusterMaxLong;
-        double averageClusterLat;
-        double averageClusterLong;
+
 
         if (!workspace.isValid()) {
             throw new BadRequestException("limitation point are invalid");
         }
 
-        System.out.println(workspace);
-        Workspace w = Workspace.builder()
-                .name(workspace.getName())
-                .clusters(new ArrayList<>())
-                .build();
-
-        latDiff = coordinatesRound(getDiff(workspace.getMinLat(), workspace.getMaxLat()));
-        longDiff = coordinatesRound(getDiff(workspace.getMinLong(), workspace.getMaxLong()));
-
-        squareSize = getGridSquareSize(latDiff, longDiff);
-
-        System.out.println("Square size :" +squareSize);
-
-        for (double i = workspace.getMinLat() ; i < (workspace.getMaxLat()); i+= squareSize){
-            for (double j = workspace.getMinLong() ; j < workspace.getMaxLong(); j+= squareSize){
-                ArrayList<Location> locations = new ArrayList<>();
-
-                clusterMinLat = i;
-                clusterMaxLat = (i + squareSize < workspace.getMaxLat() ? i + squareSize: workspace.getMaxLat());
-                clusterMinLong = j;
-                clusterMaxLong = (j + squareSize < workspace.getMaxLat() ? j + squareSize: workspace.getMaxLat());
-
-                averageClusterLat = 0;
-                averageClusterLong = 0;
-
-                Cluster cluster = Cluster.builder()
-                        .latitude(averageClusterLat)
-                        .longitude(averageClusterLong)
-                        .area("Temp")
-                        .locations(locations)
-                        .workspace(w)
-                        .build();
-
-                for(CreateLocationDTO locationDTO : workspace.getLocations()){
-                    if (between(locationDTO.getLatitude(), clusterMinLat, clusterMaxLat)
-                        && between(locationDTO.getLongitude(), clusterMinLong, clusterMaxLong)){
-                        Location location = Location.builder()
-                                .cluster(cluster)
-                                .latitude(locationDTO.getLatitude())
-                                .longitude(locationDTO.getLongitude())
-                                .build();
-
-                        locations.add(location);
-                        averageClusterLat += locationDTO.getLatitude();
-                        averageClusterLong += locationDTO.getLongitude();
-                    }
-                }
-
-                averageClusterLat = locations.size() == 0 ? 0 : averageClusterLat /  locations.size();
-                averageClusterLong = locations.size() == 0 ? 0 : averageClusterLong /  locations.size();
-
-                cluster.setLatitude(averageClusterLat);
-                cluster.setLongitude(averageClusterLong);
-
-                // TODO: Pour chaque cluster, tapper sur l'api de Geocoding pour pouvoir récupérer le nom du clister
-
-                w.getClusters().add(cluster);
-            }
-        }
+        Workspace w = this.workspaceService.newWorkspace(workspace);
 
 
         Integer workspaceId = this.workspaceService.createWorkspace(w);
@@ -161,36 +95,4 @@ public class WorkspaceController {
                 .build();
     }
 
-
-
-    private boolean between(double value, double min, double max){
-        return value >= min && value < max;
-    }
-
-    private double getGridSquareSize(double latDiff, double longDiff) {
-        double magnitude = Math.log10(Math.max(latDiff, longDiff));
-
-        System.out.println("Magnitude : "+magnitude);
-
-        return Math.pow(10,((int) Math.round(magnitude) )- 1);
-    }
-
-    private double greatestCommonDivisor(double num1, double num2){
-        BigInteger int1 = BigDecimal.valueOf(num1 * ACCURACY).toBigInteger();
-        BigInteger int2 = BigDecimal.valueOf(num2 * ACCURACY).toBigInteger();
-        BigInteger gcd = int1.gcd(int2);
-        System.out.println("GCD INT : "+ gcd);
-        BigDecimal gcdDecimal = new BigDecimal(gcd);
-        System.out.println("GCD DEC : "+ gcdDecimal);
-        return (gcdDecimal.doubleValue()) / ACCURACY;
-    }
-
-
-    private double coordinatesRound(double coordinates){
-        return (double) Math.round(coordinates * 100000d) / 100000d;
-    }
-
-    private double getDiff(double min, double max){
-        return Math.abs(max - min);
-    }
 }
--- src/main/java/com/chizu/tsuru/api/workspaces/services/WorkspaceService.java
+++ src/main/java/com/chizu/tsuru/api/workspaces/services/WorkspaceService.java
@@ -1,5 +1,9 @@
 package com.chizu.tsuru.api.workspaces.services;
 
+import com.chizu.tsuru.api.clusters.entities.Cluster;
+import com.chizu.tsuru.api.clusters.entities.Location;
+import com.chizu.tsuru.api.workspaces.dto.CreateLocationDTO;
+import com.chizu.tsuru.api.workspaces.dto.CreateWorkspaceDTO;
 import com.chizu.tsuru.api.workspaces.dto.GetWorkspaceDTO;
 import com.chizu.tsuru.api.workspaces.entities.Workspace;
 import com.chizu.tsuru.api.workspaces.repositories.WorkspaceRepository;
@@ -8,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +20,8 @@ import java.util.stream.Collectors;
 public class WorkspaceService {
 
     private final WorkspaceRepository workspaceRepository;
+    public final int LATITUDE = 0;
+    public final int LONGITUDE = 1;
 
     @Autowired
     public WorkspaceService(WorkspaceRepository workspaceRepository) {
@@ -34,5 +41,107 @@ public class WorkspaceService {
     public Integer createWorkspace(@Validated Workspace w) {
         Workspace created = workspaceRepository.save(w);
         return created.getWorkspace_id();
+    }
+
+    public double getGridSquareSize(double latDiff, double longDiff) {
+        double magnitude = Math.log10(Math.max(latDiff, longDiff));
+
+        return Math.pow(10,((int) Math.round(magnitude) )- 1);
+    }
+
+    public double coordinatesRound(double coordinates){
+        return (double) Math.round(coordinates * 100000d) / 100000d;
+    }
+
+    public double getAbsDiff(double min, double max, int side){
+
+        switch(side){
+            case LATITUDE:
+                return Math.abs(min - max);
+
+            case LONGITUDE:
+                if (min > max) {
+                    return 360 - (min  - max);
+                } else {
+                    return max - min;
+                }
+
+            default:
+                return 0;
+        }
+    }
+
+    public double getSquareSize(CreateWorkspaceDTO workspace){
+        double latDiff = coordinatesRound(getAbsDiff(workspace.getMinLat(), workspace.getMaxLat(), LATITUDE));
+        double longDiff = coordinatesRound(getAbsDiff(workspace.getMinLong(), workspace.getMaxLong(), LONGITUDE));
+
+        return getGridSquareSize(latDiff, longDiff);
+    }
+
+    public Workspace newWorkspace(CreateWorkspaceDTO workspace) {
+        double squareSize;
+        double clusterMinLat;
+        double clusterMaxLat;
+        double clusterMinLong;
+        double clusterMaxLong;
+        double averageClusterLat = 0;
+        double averageClusterLong = 0;
+
+        Workspace w = Workspace.builder()
+                .name(workspace.getName())
+                .clusters(new ArrayList<>())
+                .build();
+
+        squareSize = this.getSquareSize(workspace);
+
+        for (double i = workspace.getMinLat() ; i < (workspace.getMaxLat()); i+= squareSize){
+            for (double j = workspace.getMinLong() ; j < workspace.getMaxLong(); j+= squareSize){
+                ArrayList<Location> locations = new ArrayList<>();
+
+                clusterMinLat = i;
+                clusterMaxLat = (i + squareSize < workspace.getMaxLat() ? i + squareSize: workspace.getMaxLat());
+                clusterMinLong = j;
+                clusterMaxLong = (j + squareSize < workspace.getMaxLat() ? j + squareSize: workspace.getMaxLat());
+
+                Cluster cluster = Cluster.builder()
+                        .latitude(averageClusterLat)
+                        .longitude(averageClusterLong)
+                        .area("Temp")
+                        .locations(locations)
+                        .workspace(w)
+                        .build();
+
+                for(CreateLocationDTO locationDTO : workspace.getLocations()){
+                    if (between(locationDTO.getLatitude(), clusterMinLat, clusterMaxLat)
+                            && between(locationDTO.getLongitude(), clusterMinLong, clusterMaxLong)){
+                        Location location = Location.builder()
+                                .cluster(cluster)
+                                .latitude(locationDTO.getLatitude())
+                                .longitude(locationDTO.getLongitude())
+                                .build();
+
+                        locations.add(location);
+                        averageClusterLat += locationDTO.getLatitude();
+                        averageClusterLong += locationDTO.getLongitude();
+                    }
+                }
+
+                averageClusterLat = locations.size() == 0 ? 0 : averageClusterLat /  locations.size();
+                averageClusterLong = locations.size() == 0 ? 0 : averageClusterLong /  locations.size();
+
+                cluster.setLatitude(averageClusterLat);
+                cluster.setLongitude(averageClusterLong);
+
+                // TODO: Pour chaque cluster, tapper sur l'api de Geocoding pour pouvoir récupérer le nom du clister
+                if(locations.size() != 0)
+                    w.getClusters().add(cluster);
+            }
+        }
+
+        return w;
+    }
+
+    private boolean between(double value, double min, double max){
+        return value >= min && value < max;
     }
 }
