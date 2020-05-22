dec3b4eb5b30ff79d5b35be9e4617542a357578b
Luis Valdez <luis.valdez@affluences.com>
2020-05-22 22:34:54 +0200

    refacto

--- src/main/java/com/chizu/tsuru/api/clusters/controllers/ClusterController.java
+++ src/main/java/com/chizu/tsuru/api/clusters/controllers/ClusterController.java
@@ -1,31 +1,41 @@
 package com.chizu.tsuru.api.clusters.controllers;
 
 import com.chizu.tsuru.api.clusters.dto.GetClusterDTO;
+import com.chizu.tsuru.api.clusters.dto.GetTravelDTO;
+import com.chizu.tsuru.api.clusters.entities.Cluster;
+import com.chizu.tsuru.api.clusters.entities.Location;
 import com.chizu.tsuru.api.clusters.repositories.ClusterRepository;
-import com.chizu.tsuru.api.shared.exceptions.NotFoundException;
+import com.chizu.tsuru.api.clusters.services.ClusterService;
+import com.chizu.tsuru.api.clusters.services.TravelService;
 import com.chizu.tsuru.api.shared.services.ResponseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clusters")
 public class ClusterController {
 
-    private final ClusterRepository clusterRepository;
     private final ResponseService responseService;
+    private final ClusterService clusterService;
+    private final TravelService travelService;
 
-    public ClusterController(ClusterRepository clusterRepository, ResponseService responseService) {
-        this.clusterRepository = clusterRepository;
+    public ClusterController(ResponseService responseService, ClusterService clusterService,
+                             TravelService travelService) {
         this.responseService = responseService;
+        this.clusterService = clusterService;
+        this.travelService = travelService;
     }
 
     @GetMapping("/{idCluster}")
     public GetClusterDTO getCluster(@PathVariable("idCluster") Integer idCluster) {
-        var cluster = this.clusterRepository.findById(idCluster)
-                .orElseThrow(() -> new NotFoundException(idCluster + ": this cluster has not been found"));
-        return this.responseService.getClusterDTO(cluster) ;
+        return this.responseService.getClusterDTO(this.clusterService.getCluster(idCluster)) ;
+    }
+
+    @GetMapping("/{idCluster}/travel")
+    public GetTravelDTO getPath(@PathVariable("idCluster") Integer idCluster,
+                                @RequestParam() Double longitude,
+                                @RequestParam() Double latitude) {
+
+        return this.travelService.getTravel(idCluster, longitude, latitude);
     }
 
 }
